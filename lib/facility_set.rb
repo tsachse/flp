ef56a9709ee924fe51d3c1a521fa88965ba8ca53
sachse_t <thomas.sachse@ottogroup.com>
2016-06-12 16:46:13 +0200

    Entwurf FacilitySet..

---
+++ lib/facility_set.rb
@@ -0,0 +1,22 @@
+require 'facility'
+
+class FacilitySet
+  attr_reader :facilities
+  attr_reader :width
+  attr_reader :height
+  attr_accessor :x1
+  attr_accessor :y1
+  attr_accessor :x2
+  attr_accessor :y2
+
+  def initialize(facilities)
+    @facilities = facilities
+    @width = @height = 0
+    @facilities.each do |f|
+      @width = @width + f. width
+      @height = @height + f.height
+    end
+  end
+
+end
+
