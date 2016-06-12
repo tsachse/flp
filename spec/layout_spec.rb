84660ad41d265ccdd62c6cd429402b111efdfc1a
sachse_t <thomas.sachse@ottogroup.com>
2016-06-12 16:44:53 +0200

    Erste Implemntierung des Layouts

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -1,12 +1,33 @@
 require 'spec_helper'
 
-describe 'Layout erzeugen' do
-  it "Klasse erzeugen" do
+describe Layout do
+  it "new instance" do
     facilities = [2,6,3,4,5,0,1]
     silicing_order = [4,3,2,1,0,5]
     orientation = [0,0,1,1,0]
     l = Layout.new(facilities,silicing_order,orientation)
 
     expect(l.class).to eq(Layout)
+    expect(l.facility_set).to eq([[2], [6], [3], [4], [5], [0], [1]])
+  end
+
+  it "a horizontal cut" do
+    facilities = [2,6,3,4,5,0,1]
+    silicing_order = [4]
+    orientation = [0]
+    l = Layout.new(facilities,silicing_order,orientation)
+
+    expect(l.class).to eq(Layout)
+    expect(l.facility_set).to eq([[2, 6, 3, 4, 5], [0, 1]])
+  end
+
+  it "a vertical cut" do
+    facilities = [2,6,3,4,5,0,1]
+    silicing_order = [4]
+    orientation = [1]
+    l = Layout.new(facilities,silicing_order,orientation)
+
+    expect(l.class).to eq(Layout)
+    expect(l.facility_set).to eq([[2, 6, 3, 4, 5], [0, 1]])
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,2 @@
 require File.dirname(__FILE__) + '/../lib/layout.rb'
+require File.dirname(__FILE__) + '/../lib/facility.rb'
