8fdebec2f2a43b82a78f9104398c38907040e50a
sachse_t <thomas.sachse@ottogroup.com>
2016-06-27 11:33:18 +0200

    Testdaten schoen gemacht.

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -15,7 +15,7 @@ describe Layout do
 
   it "new instance" do
     silicing_order = [4,3,2,1,0,5]
-    orientation = [0,0,1,1,0]
+    orientation =    [0,0,1,1,1,0]
     l = Layout.new(@facilities,silicing_order,orientation)
 
     expect(l.class).to eq(Layout)
@@ -73,7 +73,7 @@ describe Layout do
 
   it "two cuts" do
     silicing_order = [4,3]
-    orientation = [0,0,1,1,0]
+    orientation    = [0,0,1,1,1,0]
     l = Layout.new(@facilities,silicing_order,orientation)
 
     expect(l.class).to eq(Layout)
@@ -82,11 +82,29 @@ describe Layout do
 
   it "three cuts" do
     silicing_order = [4,3,2]
-    orientation = [0,0,1,1,0]
+    orientation    = [0,0,1,1,1,0]
     l = Layout.new(@facilities,silicing_order,orientation)
 
     expect(l.class).to eq(Layout)
     img = SVGHelper.layout_to_svg_file(l,@output_path_pattern + 'three_cuts.svg')
+  end
+
+  it "four cuts" do
+    silicing_order = [4,3,2,1]
+    orientation    = [0,0,1,1,1,0]
+    l = Layout.new(@facilities,silicing_order,orientation)
+
+    expect(l.class).to eq(Layout)
+    img = SVGHelper.layout_to_svg_file(l,@output_path_pattern + 'four_cuts.svg')
+  end
+
+  it "five cuts" do
+    silicing_order = [4,3,2,1,0]
+    orientation    = [0,0,1,1,1,0]
+    l = Layout.new(@facilities,silicing_order,orientation)
+
+    expect(l.class).to eq(Layout)
+    img = SVGHelper.layout_to_svg_file(l,@output_path_pattern + 'five_cuts.svg')
   end
 
 end
