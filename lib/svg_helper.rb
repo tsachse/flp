6cf45b52a94f291b559a79583755830ffc3c703b
sachse_t <thomas.sachse@ottogroup.com>
2016-07-20 11:39:13 +0200

    SVG skaliert.

--- lib/svg_helper.rb
+++ lib/svg_helper.rb
@@ -6,17 +6,18 @@ class SVGHelper
     colors = ['red','green','blue','yellow','brown','pink','cyan','gray']
     i = 0
     tmp = FacilitySet.new(layout.facilities) 
-    img = Rasem::SVGImage.new(tmp.width,tmp.height) do
+    r = 800.0 / tmp.width 
+    img = Rasem::SVGImage.new(tmp.width * r ,tmp.height * r) do
       group :stroke=>"black" do
 	layout.facility_set.each do |fs|
-	  rectangle fs.x1, 
-	    fs.y1, 
-	    fs.width, 
-	    fs.height, 
+	  rectangle fs.x1 * r, 
+	    fs.y1 * r, 
+	    fs.width * r, 
+	    fs.height * r, 
 	    :stroke_width=>1, :fill=>colors[i%colors.size]
-	  tx = fs.x1 + fs.width / 2
-	  ty = fs.y1 + fs.height / 2
-	  text tx, ty, fs.label
+	  tx = fs.x1 * r + fs.width / 2 * r
+	  ty = fs.y1 * r + fs.height / 2 * r
+	  text tx, ty, fs.label, {"font-size" => 12}
 	  i = i + 1
 	end
       end
