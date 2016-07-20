6b109f50745244f0331f406eb3169e8ec52ea455
sachse_t <thomas.sachse@ottogroup.com>
2016-07-20 15:03:45 +0200

    Logging verbessert.

--- datasets/dataset_helper.rb
+++ datasets/dataset_helper.rb
@@ -8,6 +8,7 @@ require File.dirname(__FILE__) + '/../lib/variable_neighborhood_search.rb'
 require File.dirname(__FILE__) + '/../lib/material_flow.rb'
 
 require 'logger'
+require 'json'
 
 class DatasetHelper 
   def self.run(dataset_name, facilities, material_flow, neighbours, max_no_improv, max_no_improv_ls)
@@ -15,27 +16,45 @@ class DatasetHelper
     output_path_pattern = File.dirname(__FILE__) + '/output/' + dataset_name + '_'
 
     logger = Logger.new(fn_logfile)
-    logger.info("Start " +  dataset_name + " ****************************")
-    logger.info('facilities: ' + facilities.inspect.to_s)
-    logger.info('material_flow: ' + material_flow.inspect.to_s)
-    logger.info('neighbours: ' + neighbours.inspect.to_s)
-    logger.info('max_no_improv: ' + max_no_improv.inspect.to_s)
-    logger.info('max_no_improv_ls: ' + max_no_improv_ls.inspect.to_s)
+    logger.debug("Start " +  dataset_name + " ****************************")
+    logger.debug('facilities: ' + facilities.inspect.to_s)
+    logger.debug('material_flow: ' + material_flow.inspect.to_s)
+    logger.debug('neighbours: ' + neighbours.inspect.to_s)
+    logger.debug('max_no_improv: ' + max_no_improv.inspect.to_s)
+    logger.debug('max_no_improv_ls: ' + max_no_improv_ls.inspect.to_s)
 
+    t1 = Time.now
     vns = VariableNeighborhoodSearch.new(facilities, material_flow)
     best = vns.search(neighbours,max_no_improv, max_no_improv_ls)
+    t2 = Time.now
     initial = vns.initial_layout
 
-    logger.info('vns.iter: ' + vns.iter.inspect.to_s)
-    logger.info('vns.best_iter: ' + vns.best_iter.inspect.to_s)
-    logger.info('initial.material_flow.costs: ' + initial.material_flow.costs.inspect.to_s)
-    logger.info('initial.material_flow.costs: ' + initial.material_flow.costs.inspect.to_s)
-    logger.info('initial.material_flow.distance: ' + initial.material_flow.distance.inspect.to_s)
-    logger.info('initial.material_flow.facility_map: ' + initial.material_flow.facility_map.inspect.to_s)
-    logger.info('best.material_flow.costs: ' + best.material_flow.costs.inspect.to_s)
-    logger.info('best.material_flow.distance: ' + best.material_flow.distance.inspect.to_s)
-    logger.info('best.material_flow.facility_map: ' + best.material_flow.facility_map.inspect.to_s)
-    logger.info("Stop  " +  dataset_name + " ****************************")
+    logger.debug('vns.iter: ' + vns.iter.inspect.to_s)
+    logger.debug('vns.best_iter: ' + vns.best_iter.inspect.to_s)
+    logger.debug('initial.material_flow.costs: ' + initial.material_flow.costs.inspect.to_s)
+    logger.debug('initial.material_flow.distance: ' + initial.material_flow.distance.inspect.to_s)
+    logger.debug('initial.material_flow.facility_map: ' + initial.material_flow.facility_map.inspect.to_s)
+    logger.debug('best.material_flow.costs: ' + best.material_flow.costs.inspect.to_s)
+    logger.debug('best.material_flow.distance: ' + best.material_flow.distance.inspect.to_s)
+    logger.debug('best.material_flow.facility_map: ' + best.material_flow.facility_map.inspect.to_s)
+    logger.debug('duration: ' + (t2 - t1).to_s)
+
+    s = Hash.new
+    s['dataset'] = dataset_name
+    s['duration'] = t2-t1
+    s['neighbours'] = neighbours
+    s['max_no_improv'] = max_no_improv
+    s['max_no_improv_ls'] = max_no_improv_ls
+    s['iterations'] = vns.iter.inspect
+    s['best_iteration'] = vns.best_iter.inspect
+    s['initial_costs'] = initial.material_flow.costs
+    s['initial_distance'] = initial.material_flow.distance
+    s['best_costs'] = best.material_flow.costs
+    s['best_distance'] = best.material_flow.distance;
+    logger.info(JSON.generate(s));
+
+
+    logger.debug("Stop  " +  dataset_name + " ****************************")
     logger.close
 
     SVGHelper.layout_to_svg_file(initial,output_path_pattern + 'initial_layout.svg')
