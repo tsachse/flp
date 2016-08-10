7816e59f3c759df20726c7294f4029ccc7164523
Thomas Sachse <thomas.sachse@web.de>
2016-08-10 17:30:14 +0200

    ..

--- doc/160805/skas8_boxplot.R
+++ doc/160805/skas8_boxplot.R
@@ -4,9 +4,17 @@ floyd <- read.csv('dataset_skas_8f.log.csv')
 simple <- read.csv('dataset_skas_8s.log.csv')
 p <- simple[,"dataset"]=="skas_8s+"
 simple_plus <- simple[p,]
+par(mfcol=c(1,2))
 boxplot(dijkstra$duration,
         floyd$duration,
         simple_plus$duration,
-        main="Boxplot der Rechenzeit",
+        main="Rechenzeit",
         ylab="Sekunden",
         xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
+boxplot(dijkstra$best_costs,
+        floyd$best_costs,
+        simple_plus$best_costs,
+        main="Kosten",
+        ylab="Kosten",
+        xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
+
