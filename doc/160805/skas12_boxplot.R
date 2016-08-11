9cbb91929740006968dd1f555314c88a352f5df2
Thomas Sachse <thomas.sachse@web.de>
2016-08-11 17:55:35 +0200

    R-Script erzeugt png

--- doc/160805/skas12_boxplot.R
+++ doc/160805/skas12_boxplot.R
@@ -4,17 +4,18 @@ floyd <- read.csv('dataset_skas_12f.log.csv')
 simple <- read.csv('dataset_skas_12s.log.csv')
 p <- simple[,"dataset"]=="skas_12s+"
 simple_plus <- simple[p,]
+png(filename="dataset_skas_12_boxplot.png")
 par(mfcol=c(1,2))
 boxplot(dijkstra$duration,
         floyd$duration,
         simple_plus$duration,
         main="Rechenzeit",
         ylab="Sekunden",
-        xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
+        xlab="Variante",names=c("D","F","S"))
 boxplot(dijkstra$best_costs,
         floyd$best_costs,
         simple_plus$best_costs,
         main="Kosten",
         ylab="Kosten",
-        xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
-
+        xlab="Variante",names=c("D","F","S"))
+dev.off()
