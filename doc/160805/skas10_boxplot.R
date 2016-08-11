1d7a5e5eec89b5a1ce6c0059d189ac2ceff828c2
Thomas Sachse <thomas.sachse@web.de>
2016-08-11 17:47:34 +0200

    Neue CSV und R-Scripts

---
+++ doc/160805/skas10_boxplot.R
@@ -0,0 +1,20 @@
+#  R --save < skas10_costs_boxplot.R
+dijkstra <- read.csv('dataset_skas_10.log.csv')
+floyd <- read.csv('dataset_skas_10f.log.csv')
+simple <- read.csv('dataset_skas_10s.log.csv')
+p <- simple[,"dataset"]=="skas_10s+"
+simple_plus <- simple[p,]
+par(mfcol=c(1,2))
+boxplot(dijkstra$duration,
+        floyd$duration,
+        simple_plus$duration,
+        main="Rechenzeit",
+        ylab="Sekunden",
+        xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
+boxplot(dijkstra$best_costs,
+        floyd$best_costs,
+        simple_plus$best_costs,
+        main="Kosten",
+        ylab="Kosten",
+        xlab="Variante",names=c("Dijkstra","Floyd","Simple"))
+
